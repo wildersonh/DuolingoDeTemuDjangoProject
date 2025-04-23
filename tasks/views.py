d536e9ee246ef2b4dec7051f1612c1282b9946d5
wildersonh <wildersonhernandez994@gmail.com>
2025-04-22 20:38:57 -0400

    Modificacion de las tareas

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .form import TaskForm
+from .models import Task
 # Create your views here.
 
 def home(request):
@@ -35,7 +36,10 @@ def signup(request):
         })
         
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, datacompleted__isnull=True)
+    return render(request, 'tasks.html',{
+        'tasks': tasks
+    })
 
 def create_tasks(request):
     if request.method == 'GET':
@@ -43,10 +47,19 @@ def create_tasks(request):
             'form': TaskForm
         })
     else:
-        print(request.POST)
-        return render(request, 'create_tasks.html', {
-            'form': TaskForm
-        })
+        try:
+            form = TaskForm(request.POST)
+            new_tasks = form.save(commit=False)
+            new_tasks.user = request.user
+            new_tasks.save()
+            return redirect('tasks')
+        except ValueError:
+
+            return render(request, 'create_tasks.html', {
+                'form': TaskForm,
+                'error': 'No se guardo correctamente'
+            })
+
 
 def cerrar_sesion(request):
     logout(request)
